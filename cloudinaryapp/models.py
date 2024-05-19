7c8389d70c6c3c7a61172515d0ce9b0eb19768c2
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-05-19 20:15:12 +0900

    models.pyでPhotoモデルを作成

--- cloudinaryapp/models.py
+++ cloudinaryapp/models.py
@@ -1,11 +1,25 @@
-# models.py
-
 from django.db import models
 from cloudinary.models import CloudinaryField
 
 class Photo(models.Model):
+    """
+    画像を保存するためのDjangoモデル。
+
+    このモデルは、タイトルとCloudinaryに保存される画像を持ちます。
+
+    Attributes:
+        title (str): 画像のタイトル。最大100文字。
+        image (CloudinaryField): Cloudinaryに保存される画像。
+    """
+
     title = models.CharField(max_length=100)
     image = CloudinaryField('image')
 
     def __str__(self):
+        """
+        モデルインスタンスの文字列表現を返します。
+
+        Returns:
+            str: 画像のタイトル。
+        """
         return self.title
