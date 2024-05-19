cc71d7ee1201fcf4327d3534251754c6e5fbb8d4
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-05-19 20:16:03 +0900

    admin.pyでPhotoモデルを管理出来るよう追加

--- cloudinaryapp/admin.py
+++ cloudinaryapp/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Photo
+
+admin.site.register(Photo)
+"""
+Photoモデルの管理を行うための管理サイト設定。
+
+このモジュールは、Djangoの管理サイトでPhotoモデルを編集・表示するための設定を提供します。
+Photoモデルは、タイトルと画像を持つモデルであり、管理サイト上での操作を可能にします。
+"""
