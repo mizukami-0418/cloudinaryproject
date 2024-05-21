9d8bcbd6b0cf459b9d7869959313a418ca44574e
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2024-05-21 23:35:40 +0900

    URLの設定とテンプレートの作成

--- cloudinaryapp/urls.py
+++ cloudinaryapp/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.photo_list, name='photo_list'),
     path('upload/', views.upload_photo, name='upload_photo'),
+    path('album/new/', views.create_album, name='create_album'),
+    path('album/<int:pk>/', views.album_detail, name='album_detail'),
 ]
